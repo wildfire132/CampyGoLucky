01ec8aaa473d9eca58dab51bf567a75887d9f17b
ayerest <irisiseve@gmail.com>
2019-09-10 11:28:23 -0700

    started work on the trip info page

---
+++ campy-go-lucky-frontend/assets/tripPage.js
@@ -0,0 +1,42 @@
+function displayTrip(trip) {
+    let renderDelete = document.querySelector(".render-delete")
+    deleteAllUnder(renderDelete)
+    let tripName = document.createElement("h2")
+    tripName.innerText = `${trip.name}`
+
+    let campSitesList = document.createElement("ul")
+    trip.campsites.forEach(campsite => {
+        let campspot = document.createElement("li")
+        let campspotInfoLink = document.createElement("a")
+        campspotInfoLink.innerText = campsite.name
+        campspotInfoLink.addEventListener("click", displayCampSiteInfo(campsite))
+
+        let dltBtn = document.createElement("button")
+        dltBtn.innerText = "Delete"
+        dltBtn.addEventListener("click", deleteCampSite(campsite))
+
+        campspot.appendChild(campspotInfoLink)
+        campspot.appendChild(dltBtn)
+
+        campSitesList.appendChild(campspot)
+    })
+
+
+
+
+}
+
+async function deleteCampSite(campsite) {
+    return function(e) {
+        let campsiteId = campsite.id
+        await fetch(`http://localhost:3000/campsites/${campsiteId}`, {
+            method: "DELETE"
+        })
+    }
+}
+
+function displayCampSiteInfo(campsite) {
+    return function(e) {
+        debugger
+    }
+}
