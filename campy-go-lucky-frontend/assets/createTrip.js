0355be935c505773c7491b1700a22d561cd5abf5
github username <github email address>
2019-09-10 11:43:18 -0700

    created new trip form

--- campy-go-lucky-frontend/assets/createTrip.js
+++ campy-go-lucky-frontend/assets/createTrip.js
@@ -17,4 +17,86 @@ function createTrip(){
         <button type="submit" class="btn btn-outline-success">Create Trip!</button>
     </form>
 </div> */
+
+let renderDelete = document.querySelector(".render-delete")
+deleteAllUnder(renderDelete)
+
+let jumbo = document.createElement("div")
+jumbo.classList.add("jumbotron")
+
+let appTitle = document.createElement("h1")
+appTitle.classList.add("display-4")
+appTitle.innerText = "Creating New Trip"
+
+let appDesc = document.createElement("p")
+appDesc.classList.add("lead")
+appDesc.innerText = "Let us help you plan your next camping trip! Plan your road trip to get to your favorite camping destinations and add campsites and hiking trails for when you get there. Campy Go Lucky is here for all your camping needs!"
+
+let horizRule = document.createElement("hr")
+horizRule.classList.add("my-4")
+
+let loginForm = document.createElement("form")
+loginForm.onsubmit = e =>{
+    e.preventDefault()
+    createFormSubmission("theresa")
+}
+
+let formStyle = document.createElement("div")
+formStyle.class = "form-group"
+
+let tripLabel = document.createElement("label")
+tripLabel.for = "exampleInputEmail1"
+tripLabel.innerText = "Username"
+let tripInput = document.createElement("input")
+tripInput.type = "text"
+tripInput.classList.add("form-control")
+tripInput.id = "exampleInputEmail1"
+tripInput.placeholder = "Enter username"
+tripInput.setAttribute("aria-describedby","emailHelp")
+
+let startingLocationLabel = document.createElement("label")
+startingLocationLabel.for = "inputStartLocation"
+startingLocationLabel.innerText = "Starting Location"
+let startingLocationInput = document.createElement("input")
+startingLocationInput.type = "text"
+startingLocationInput.classList.add("form-control")
+startingLocationInput.id = "exampleInputEmail1"
+startingLocationInput.placeholder = "Enter starting location"
+startingLocationInput.setAttribute("aria-describedby","emailHelp")
+
+let startDateLabel = document.createElement("label")
+startDateLabel.for = "inputDate"
+startDateLabel.innerText = "Enter Start Date"
+let startDateInput = document.createElement("input")
+startDateInput.classList.add("form-control")
+startDateInput.type = "date"
+startDateInput.id = "inputDate"
+startDateInput.placeholder = "Enter date"
+startDateInput.setAttribute("aria-describedby","emailHelp")
+
+let submitBtn = document.createElement("button")
+submitBtn.classList.add("btn", "btn-outline-success")
+submitBtn.type = "submit"
+submitBtn.innerText = "Let's Go!"
+
+formStyle.appendChild(tripLabel)
+formStyle.appendChild(tripInput)
+formStyle.appendChild(startingLocationLabel)
+formStyle.appendChild(startingLocationInput)
+formStyle.appendChild(startDateLabel)
+formStyle.appendChild(startDateInput)
+formStyle.appendChild(submitBtn)
+
+loginForm.appendChild(formStyle)
+
+jumbo.appendChild(appTitle)
+jumbo.appendChild(appDesc)
+jumbo.appendChild(horizRule)
+jumbo.appendChild(loginForm)
+
+renderDelete.appendChild(jumbo)
+}
+
+function createFormSubmission(string){
+console.log(string)
 }
--- campy-go-lucky-frontend/assets/loginPage.js
+++ campy-go-lucky-frontend/assets/loginPage.js
@@ -1,8 +1,21 @@
 function loginPage(){
+// <div class="jumbotron">
+// <h1 class="display-4">Campy Go Lucky</h1>
+// <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+// <hr class="my-4">
+// <form>
+//     <div class="form-group">
+//         <label for="exampleInputEmail1">Username</label>
+//         <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter name">
+//         <small id="emailHelp" class="form-text text-muted">We'll never share your username with anyone else.</small>
+//     </div>
+//     <button type="submit" class="btn btn-outline-success">Let's Go!</button>
+// </form>
+// </div>
+
     let renderDelete = document.querySelector(".render-delete")
-    while (renderDelete.firstChild) {
-        renderDelete.firstChild.remove()
-    }
+    deleteAllUnder(renderDelete)
+
     let jumbo = document.createElement("div")
     jumbo.classList.add("jumbotron")
 
@@ -18,45 +31,53 @@ function loginPage(){
     horizRule.classList.add("my-4")
 
     let loginForm = document.createElement("form")
-    loginForm.id = "login"
+    loginForm.onsubmit = e =>{
+        e.preventDefault()
+        loginFormSubmission("bob")
+    }
+
+    let formStyle = document.createElement("div")
+    formStyle.class = "form-group"
 
     let userLabel = document.createElement("label")
-    // userEmailLabel.for = "username"
+    userLabel.for = "exampleInputEmail1"
+    userLabel.innerText = "Username"
 
     let userNameInput = document.createElement("input")
     userNameInput.type = "text"
     userNameInput.classList.add("form-control")
     userNameInput.id = "exampleInputEmail1"
     userNameInput.placeholder = "Enter username"
+    userNameInput.setAttribute("aria-describedby","emailHelp")
+
+    let helpText = document.createElement("small")
+    helpText.id = "emailHelp"
+    helpText.classList.add("form-text", "text-muted")
+    helpText.innerText = "We'll never share your username with anyone else."
 
     let submitBtn = document.createElement("button")
-    submitBtn.classList.add("btn, btn-outline-success")
+    submitBtn.classList.add("btn", "btn-outline-success")
     submitBtn.type = "submit"
     submitBtn.innerText = "Let's Go!"
 
-    loginForm.appendChild(userEmailLabel)
-    loginForm.appendChild(userNameInput)
-    loginForm.appendChild(submitBtn)
+    userNameInput.appendChild(helpText)
+
+    formStyle.appendChild(userLabel)
+    formStyle.appendChild(userNameInput)
+    formStyle.appendChild(submitBtn)
+
+    loginForm.appendChild(formStyle)
 
     jumbo.appendChild(appTitle)
     jumbo.appendChild(appDesc)
     jumbo.appendChild(horizRule)
     jumbo.appendChild(loginForm)
+    
+    renderDelete.appendChild(jumbo)
+}
 
-
-
-// <div class="jumbotron">
-// <h1 class="display-4">Campy Go Lucky</h1>
-// <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-// <hr class="my-4">
-// <form>
-//     <div class="form-group">
-//         <label for="exampleInputEmail1">Username</label>
-//         <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter name">
-//         <small id="emailHelp" class="form-text text-muted">We'll never share your username with anyone else.</small>
-//     </div>
-//     <button type="submit" class="btn btn-outline-success">Let's Go!</button>
-// </form>
-// </div>
+function loginFormSubmission(string){
+    console.log(string)
+    createTrip()
 
 }
