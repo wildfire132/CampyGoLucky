34a1c0c426f988e1fa6286a8d22405221da2aab3
github username <github email address>
2019-09-11 09:39:23 -0700

    deleted user_id from serializer

--- campy-go-lucky-backend/app/serializers/trip_serializer.rb
+++ campy-go-lucky-backend/app/serializers/trip_serializer.rb
@@ -1,4 +1,4 @@
 class TripSerializer < ActiveModel::Serializer
-  attributes :id, :user_id, :name, :start_date, :start_location, :campsites, :trails
+  attributes :id, :name, :start_date, :start_location, :campsites, :trails
   has_one :user
 end
