b1f6b1ef00ac2901ea95b57a564a91a304627f8e
ayerest <irisiseve@gmail.com>
2019-09-12 17:28:51 -0700

    got search field on map page working to repopulate map based on new location --- delete button functionality removed

--- campy-go-lucky-backend/app/controllers/coms_controller.rb
+++ campy-go-lucky-backend/app/controllers/coms_controller.rb
@@ -2,7 +2,7 @@ class ComsController < ApplicationController
     def get_map
         # byebug
         search_term = params['startLocation'].gsub(" ","+")
-        string_response = RestClient.get("https://maps.googleapis.com/maps/api/geocode/json?address=#{search_term}&key=#{ENV["GOOGLE_MAPS_API_KEY"]}")
+        string_response = RestClient.get("https://maps.googleapis.com/maps/api/geocode/json?address=#{search_term}&key=#{ENV["GOOGLE_MAPS_API_"]}")
       response_hash = JSON.parse(string_response)
   
        lat_long_hash = response_hash["results"][0]["geometry"]["location"]
